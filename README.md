ee4143eb2cec27bdbb0fc01894bc1ade154d9aa6
Jagar <jagaryousef@my.uopeople.edu>
2021-10-11 21:09:21 +0300

    stable release

--- README.md
+++ README.md
@@ -1,3 +1,76 @@
 # Kurdish words corrector <br>
 
-Under update!
+Correct the typos and the Unicode problems in Kurdish (Kurmanji) by brute forcing and comparing with a dictionary.<br>
+The brute forcing has 3 different depths and specific most popular typos like writing `s` instead of `ş` or `e` instead of `ê`.
+<br><br>
+<strong>Incorrect sentence: </strong>
+```text
+Reso cu dur.
+```
+<br>
+<strong>Corrected sentence: </strong>
+
+```text
+Reşo çû dûr.
+```
+<br>
+
+### Usage: <br>
+
+```shell script
+python kurdish-words-corrector.py -t "Reso cu dur." -o "results.txt"
+```
+
+- You can read the results in `yaml` or `json` formats when you didn't include `-o` path.
+- The script will save a states file in the same path which include the `json` or `yaml` formated results (it includes some states too).
+<br>
+
+An example of the resulted `json`: <br>
+
+```json
+{
+  "correct_words": [],
+  "incorrect_words_with_possible_corrections": [
+    {
+      "word": "Reso",
+      "message": "Is not in our database, and we found similar word/s",
+      "status": 1,
+      "possibilities": [
+        "reşo"
+      ]
+    },
+    {
+      "word": "cu",
+      "message": "Is not in our database, and we found similar word/s",
+      "status": 1,
+      "possibilities": [
+        "çû"
+      ]
+    },
+    {
+      "word": "dur",
+      "message": "Is not in our database, and we found similar word/s",
+      "status": 1,
+      "possibilities": [
+        "dûr"
+      ]
+    }
+  ],
+  "incorrect_words_without_possible_corrections": [],
+  "total_words": 3,
+  "total_incorrect": 3,
+  "total_incorrect_with_corrections": 3,
+  "total_incorrect_without_corrections": 0,
+  "incorrect_percentage": 100
+}
+```
+
+#### Arguments: <br>
+
+| Argument      | Description |
+| ----------- | ----------- |
+| -t <strong>or</strong> --text    | The entered text to correct its words       |
+| -o <strong>or</strong> --output | The path of output results file with corrected text        |
+| -f <strong>or</strong> --file    | The path of the file to correct its text's words       |
+| -d <strong>or</strong> --depth | With the values 1, 2 or 3, to increase the level of brute forcing but also the time it needs to be processed       |
+| -p <strong>or</strong> --parser    | The parsing of the outputed file; yaml (default) or json       |
--- kurdish-words-corrector.py
+++ kurdish-words-corrector.py
@@ -1,17 +1,47 @@
+import json
 from itertools import product
+import argparse
+import re
 
+parser = argparse.ArgumentParser(description='Correct Kurdish words especially the ones that went through wrong '
+                                             'unicode settings')
+parser.add_argument('-w', '--word', dest='word', type=str, help='The word that you want to correct it')
+parser.add_argument('-t', '--text', dest='text', type=str, help='The text to correct its words')
+parser.add_argument('-f', '--file', dest='file', type=str, help='The file path to correct words from its text')
+parser.add_argument('-o', '--output', dest='output', type=str, help='The output file, if you want to save the results')
+parser.add_argument('-d', '--depth', dest='depth', type=int, help='values from 1-3 depend on how depth (then slow) '
+                                                                  'you want to correct, 1 is the fastest and the lest'
+                                                                  ' depth, 3 is the slowest the most depth')
+parser.add_argument('-p', '--parser', dest='parser', type=str, help='Parse the output file (json or yaml)')
 
-common_mistakes_dict = {'e': ['ê'], 'u': ['û'], 'i': ['î'], 's': ['ş'], 'c': ['ç'], 'w': ['v'],
-                        'ê': ['e'], 'û': ['u'], 'î': ['i'], 'ş': ['s'], 'ç': ['c'], 'v': ['w']}
+args = parser.parse_args()
+
+common_mistakes_dict_1 = {'e': ['ê'], 'u': ['û'], 'i': ['î'], 's': ['ş'], 'c': ['ç'], 'w': ['v'],
+                          'ê': ['e'], 'û': ['u'], 'î': ['i'], 'ş': ['s'], 'ç': ['c'], 'v': ['w']}
+
+common_mistakes_dict_2 = {'e': ['ê'], 'u': ['û', 'o', 'w'], 'i': ['î'], 's': ['ş'], 'c': ['ç'], 'w': ['v', 'o'],
+                          'ê': ['e'], 'û': ['u', 'o', 'w'], 'î': ['i'], 'ş': ['s'], 'ç': ['c'], 'v': ['w'],
+                          'o': ['u', 'û', 'w']}
+
+common_mistakes_dict_3 = {'e': ['ê', 'i'], 'u': ['û', 'o', 'w'], 'i': ['î', 'e'], 's': ['ş'], 'c': ['ç'],
+                          'w': ['v', 'o'],
+                          'ê': ['e', 'î'], 'û': ['u', 'o', 'w'], 'î': ['i', 'ê'], 'ş': ['s'], 'ç': ['c'], 'v': ['w'],
+                          'o': ['u', 'û', 'w']}
 
 f = open('correct_words.txt', 'r', encoding='utf-8')
 correct_words = f.read().split('\n')
 f.close()
 
 
-def generate_similar_correct_words(word):
+def correct_word(word, depth=1):
+    if depth == 3:
+        common_mistakes_dict = common_mistakes_dict_3
+    elif depth == 2:
+        common_mistakes_dict = common_mistakes_dict_2
+    else:
+        common_mistakes_dict = common_mistakes_dict_1
     if word.lower() in correct_words:
-        return word + ' is a correct word'
+        return {'word': word, 'message': 'Is a correct word', 'status': 0}
     else:
         for key in common_mistakes_dict.keys():
             if key not in common_mistakes_dict[key]:
@@ -19,24 +49,129 @@ def generate_similar_correct_words(word):
 
         res = []
 
-        # constructing all possible combination of values using product
-        # mapping using zip()
         for sub in [zip(common_mistakes_dict.keys(), chr) for chr in product(*common_mistakes_dict.values())]:
             temp = word.lower()
             for repls in sub:
-                # replacing all elements at once using * operator
                 temp = temp.replace(*repls)
             res.append(temp)
 
         combinations = set(res)
-        possibilities = set()
+        possibilities = []
+
         for combination in combinations:
             if combination in correct_words:
-                possibilities.add(combination)
+                possibilities.append(combination)
 
         if len(possibilities) == 0:
-            return word + ' is not in our database, and we didn\'t find a similar word'
-        return possibilities
+            return {'word': word, 'message': 'Is not in our database, and we didn\'t find a similar word',
+                    'status': 2}
+        return {'word': word, 'message': 'Is not in our database, and we found similar word/s', 'status': 1,
+                'possibilities': possibilities}
 
 
-print(generate_similar_correct_words("reso"))
+def correct_text(text, output_path=None, depth=1, parser="yaml"):
+    all_words = list(filter(None, __split_text(text).strip().split("\n")))
+    output = __get_output(all_words, depth)
+
+    if output_path is None:
+        return str(output)
+    else:
+        output_states_path = '-states.'.join(output_path.split('.', 1))
+        if not output['incorrect_words_with_possible_corrections']:
+            if parser == "json":
+                output = json.dumps(output)
+            __save_to_file(text, output_path)
+            __save_to_file(str(output), output_states_path)
+        else:
+            for item in output['incorrect_words_with_possible_corrections']:
+                word = item['word']
+                replacer = item['possibilities'][0]
+                if word[0].isupper():
+                    replacer = replacer.capitalize()
+                text = text.replace(word, replacer, 1)
+            if parser == "json":
+                output = json.dumps(output)
+            __save_to_file(text, output_path)
+            __save_to_file(str(output), output_states_path)
+
+
+def correct_file(file_path, output_path=None, depth=1, parser="yaml"):
+    text = __read_from_file(file_path)
+    return correct_text(text, output_path, depth, parser)
+
+
+def __get_output(all_words, depth=1):
+    output = dict()
+    results = []
+    current_correct_words = []
+    incorrect_words_with_possible_corrections = []
+    incorrect_words_without_possible_corrections = []
+
+    for word in all_words:
+        results.append(correct_word(word, depth))
+
+    for item in results:
+        if item['status'] == 0:
+            current_correct_words.append(item)
+        elif item['status'] == 1:
+            incorrect_words_with_possible_corrections.append(item)
+        elif item['status'] == 2:
+            incorrect_words_without_possible_corrections.append(item)
+
+    output['correct_words'] = current_correct_words
+    output['incorrect_words_with_possible_corrections'] = incorrect_words_with_possible_corrections
+    output['incorrect_words_without_possible_corrections'] = incorrect_words_without_possible_corrections
+
+    output['total_words'] = len(all_words)
+    output['total_incorrect'] = len(incorrect_words_with_possible_corrections) + 0.5 * len(
+        incorrect_words_without_possible_corrections)
+    output['total_incorrect_with_corrections'] = len(incorrect_words_with_possible_corrections)
+    output['total_incorrect_without_corrections'] = len(incorrect_words_without_possible_corrections)
+    output['incorrect_percentage'] = (output['total_incorrect'] * 100) / (output['total_words'])
+
+    return output
+
+
+def __save_to_file(text, file_path):
+    file = open(file_path, "w", encoding="UTF-8")
+    file.write(text)
+    file.close()
+
+
+def __read_from_file(file_path):
+    file = open(file_path, 'r', encoding='UTF-8')
+    result = file.read()
+    file.close()
+    return result
+
+
+def __split_text(text, delimiter="\n"):
+    kurdish_letters = "ABCÇDEÊFGHIÎJKLMNOPQRSŞTUÛVWXYZabcçdeêfghiîjklmnopqrsştuûvwxyz"
+    reg = "[^" + kurdish_letters + "]+"
+    text = re.sub(reg, '*', text).rstrip()
+    return delimiter.join(text.split('*'))
+
+
+if __name__ == "__main__":
+    depth = 1
+    if args.depth is not None:
+        depth = args.depth
+
+    parser = "yaml"
+    if args.parser is not None and (args.parser.lower() == "yaml" or args.parser.lower() == "json"):
+        parser = args.parser
+
+    if args.word is not None and len(args.word.strip()) > 0:
+        print(correct_word(args.word, depth=depth))
+    elif args.text is not None and len(args.text.strip()) > 0 and args.output is None:
+        print(correct_text(args.text, depth=depth, parser=parser))
+    elif args.text is not None and len(args.text.strip()) > 0 and args.output is not None:
+        correct_text(args.text, output_path=args.output, depth=depth, parser=parser)
+        print("Success:", "Corrected text has been saved saved to", args.output)
+    elif args.file is not None and len(args.file.strip()) > 0 and args.output is None:
+        print(correct_file(args.file, depth=depth, parser=parser))
+    elif args.file is not None and len(args.file.strip()) > 0 and args.output is not None:
+        correct_file(args.file, output_path=args.output, depth=depth, parser=parser)
+        print("Success:", "Corrected text has been saved saved to", args.output)
+    else:
+        print("Run 'python kurdish-words-corrector.py -h to find the usage'")
